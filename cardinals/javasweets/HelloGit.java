890b1d849ca08e9e5698d70279cd9fc02b2c92e2
ben lamb <benlamb@vt.edu>
2021-09-04 00:15:37 -0400

    Create package folder and HelloGit test file

---
+++ cardinals/javasweets/HelloGit.java
@@ -0,0 +1,10 @@
+package cardinals.javasweets;
+
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("HelloGit!");
+        // this is a test file to practice GitHub out on
+    }
+
+}
